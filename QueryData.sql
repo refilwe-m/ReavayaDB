b8e48147af83331703752b44ca5d00f43b951c76
Refilwe-M-Mashile <refilwe-mashile.rm@gmail.com>
2022-04-10 16:27:31 +0200

    Added UDFs (For Buses)

--- QueryData.sql
+++ QueryData.sql
@@ -1,1 +1,10 @@
---Queries
+--Queries---
+
+--Get all the buses that are not working anymore using UDF
+SELECT dbo.UDF_InactiveBuses() AS 'Buses not Working';
+
+--Get all the buses that are working
+SELECT dbo.UDF_ActiveBuses() AS 'Buses Working';
+
+--Get all the buses that need repairs using UDF
+SELECT  dbo.UDF_ToBeRepairedBuses() AS 'Buses Need to be Repaired'
--- UDFs.sql
+++ UDFs.sql
@@ -1,1 +1,34 @@
--- UDFs
+-- ReaVaya UDFs ----
+
+
+	-- Count InActive Buses
+CREATE FUNCTION UDF_InactiveBuses()
+RETURNS INT AS 
+BEGIN 
+	DECLARE @InactiveBuses INT
+	SELECT @InactiveBuses = COUNT(Active) FROM Buses WHERE Active = 0
+	RETURN @InactiveBuses
+END
+GO;
+
+	-- Count Active Buses
+CREATE FUNCTION UDF_ActiveBuses()
+RETURNS INT AS 
+BEGIN 
+	DECLARE @ActiveBuses INT
+	SELECT @ActiveBuses = COUNT(Active) FROM Buses WHERE Active = 1
+	RETURN @ActiveBuses
+END
+GO;
+
+
+
+	-- Count Buses That Need Repairs
+CREATE FUNCTION UDF_ToBeRepairedBuses()
+RETURNS INT AS 
+BEGIN 
+	DECLARE @RepBuses INT
+	SELECT @RepBuses = COUNT(HealthStatus) FROM Buses WHERE HealthStatus = 0
+	RETURN @RepBuses
+END
+;
