c9eaf045fd15db6f99572260b80811b2bf5832c2
Refilwe-M-Mashile <refilwe-mashile.rm@gmail.com>
2022-04-11 11:02:20 +0200

    Added View for Company Stats

--- Views.sql
+++ Views.sql
@@ -1,46 +1,15 @@
 --Views
 USE master;
+
 USE ReaVayaDB
 GO
 
--- User info
-CREATE view vUserInfo AS
+-- View Company Statitics
+CREATE VIEW CompanyStats AS
 SELECT
-	u.FirstName,
-	u.LastName,
-	a.Balance,
-	a.Points,
-	bk.DateOfTravel,
-	bk.TimeOfTravel,
-	bt.TypeName,
-	bt.SeatNumber,
-	br.FromCity,
-	br.ToCity,
-	s.Name
-FROM
-	Users AS u
-	LEFT JOIN Accounts AS a ON u.UserID = a.UserID
-	LEFT JOIN Bookings AS bk ON a.AccountID = bk.AccountID
-	LEFT JOIN Buses AS b ON bk.BusID = b.BusID
-	LEFT JOIN BusType AS bt ON b.TypeID = bt.TypeID
-	LEFT JOIN BusRoutes AS br ON b.RouteID = br.RouteID
-	LEFT JOIN Stations AS s ON br.RouteID = s.RouteID;
+	(SELECT COUNT(*) FROM dbo.Users) AS 'Total Clients',
+	(SELECT COUNT(*) FROM dbo.Buses) AS 'Total Buses',
+	(SELECT COUNT(*) FROM dbo.Employees) AS 'Total Employess',
+	(SELECT COUNT(*) FROM dbo.Tickets) AS 'Total Sales'
 GO
 
--- Employee info
-CREATE view vEmployeeInfo AS
-
-SELECT
-	e.FirstName,
-	e.LastName,
-	p.PositionName
- 	
-FROM Employees AS e
-LEFT JOIN Positions AS p ON e.PositionID = p.PositionID;
-GO
--- Active buses
-CREATE view ActiveBuses AS
-SELECT BusID
-FROM Buses WHERE Active = 1;
-
--- more views...
