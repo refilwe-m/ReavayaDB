474ccefe01091808fdcac44ac294d019e721969f
Refilwe-M-Mashile <refilwe-mashile.rm@gmail.com>
2022-04-18 07:07:43 +0200

    Added API for dashboard

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -8,11 +8,14 @@ app.use(express.static(path.join(__dirname + "/public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-getBuses().then(function(result) {console.log(result);});
+//getBuses().then(function(result) {console.log(result);});
 
 app.get("/", (req, res) => {
-  //res.send("Hello World");
   res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/dashboard",(req,res)=>{
+  res.sendFile(path.join(__dirname, "/public/dashboard.html"));
 });
 
 const port = 3000;
