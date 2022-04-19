85900307464c30944399b44487b075a1644e9a1b
Refilwe-M-Mashile <refilwe-mashile.rm@gmail.com>
2022-04-19 10:16:16 +0200

    Added functions to getDrivers, Employees & Buses

--- FrontEnd/src/queries.js
+++ FrontEnd/src/queries.js
@@ -1,20 +1,50 @@
 const sql = require("mssql/msnodesqlv8");
 const config = require("../config");
+//const SP = require("../../StoredProcedures.sql");
 
+//const pool = await sql.connect(config);
+
+//Connect to Database
 const dbConnect = new sql.connect(config, (err) =>
   err
     ? console.log(err)
     : console.log("connected to database: " + config.database)
 );
 
-async function getBuses() {
-    try {
-      const pool = await sql.connect(config);
-      const buses = await pool.request().query("SELECT * FROM dbo.Buses");
-      return buses.recordsets;
-    } catch (error) {
-      console.log(error);
-    }
+//Functions to interact with DB
+const getAllBuses = async ()=>{
+  try {
+    const pool = await sql.connect(config);
+    const buses = await pool.request().query("SELECT * FROM dbo.Buses");
+    return buses.recordsets[0][0];
+  } catch (error) {
+    console.log(error);
   }
-module.exports = dbConnect;
-module.exports = getBuses;
+}
+
+const getAllEmployees = async ()=>{
+  try {
+    const pool = await sql.connect(config);
+    const employees = await pool.request().query("SELECT * FROM dbo.Employees");
+    return employees.recordsets[0][0];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const getAllDrivers = async ()=>{try {
+  const pool = await sql.connect(config);
+  const drivers = await pool.request().query("SELECT * FROM dbo.Employees WHERE PositionID = 3");
+  return drivers.recordsets[0];
+} catch (error) {
+  console.log(error);
+}
+  
+}
+
+//getAllBuses().then((result) =>console.log(result));
+//getAllEmployees().then((result) =>console.log(result));
+//getAllDrivers().then((result) =>console.log(result));
+
+module.exports = {getAllBuses, getAllEmployees};
+//module.exports = getBuses;
