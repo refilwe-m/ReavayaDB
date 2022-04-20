5089a4cb1aeeee8c982b054a9b836842c670e4fc
Refilwe-M-Mashile <refilwe-mashile.rm@gmail.com>
2022-04-20 17:21:21 +0200

    Added functionality to add a bus

--- FrontEnd/public/scripts/addBus.js
+++ FrontEnd/public/scripts/addBus.js
@@ -1,5 +1,6 @@
 const typeOptions = document.getElementById("bustype");
 const busTypes = { C: 5, F: 10, T: 3 };
+let routeCode = "";
 
 typeOptions.addEventListener("change", function () {
   const selectedBusType = typeOptions.options[typeOptions.selectedIndex].value;
@@ -12,7 +13,32 @@ typeOptions.addEventListener("change", function () {
   document.getElementById("buscode").innerHTML = html;
 });
 
+const codeOptions = document.getElementById("buscode");
+//const  = document.getElementById("codes");
+
+codeOptions.addEventListener("change", function () {
+  if (codeOptions.value.trim() != "") {
+    routeCode = codeOptions.value;
+  }
+});
+
 const btnAddBus = document.getElementById("btnAddBus");
 btnAddBus.addEventListener("click", function () {
-
+  const inputData = {
+    routeCode: routeCode,
+    registration: document.getElementById("registration").value,
+    seats: document.getElementById("seats").value,
+  };
+  $.ajax({
+    url: "/addBus",
+    type: "POST",
+    data: inputData,
+    success: (res) => {
+      console.log(res);
+      alert("Bus Added Successfully");
+    },
+    error: function (res) {
+      alert("server error occurred");
+    },
+  });
 });
