0819576257716b576eb81051399b7c4fb7cc43af
Itumeleng Matheba <itumeleng@bbd.co.za>
2022-04-24 20:11:44 +0200

    Added greeting message

---
+++ FrontEnd/public/scripts/greetings.js
@@ -0,0 +1,12 @@
+const admins = {
+    "itumeleng@gmail.com": "Itumeleng",
+     "nomcebo@gmail.com": "Nomcebo",
+    "refilwe@gmail.com":"Refilwe",
+    "wisani@gmail.com":"Wisani"
+  };
+
+let Answer = localStorage.getItem("txtValue");
+console.log (admins[Answer])
+document.getElementById("Welcome-msg").innerHTML = "Welcome, " + admins[Answer] + "."
+document.getElementById("Welcome-msg").style.color = "rgb(49, 189, 199";
+ document.getElementById("Welcome-msg").style.fontSize = "2.5em";
