da8ad7a210c63b09e1c1931d4b7f202ea751250d
Itumeleng Matheba <itumeleng@bbd.co.za>
2022-04-24 23:52:50 +0200

    Responsiveness

--- FrontEnd/public/scripts/index.js
+++ FrontEnd/public/scripts/index.js
@@ -1,6 +1,5 @@
 const login = document.getElementById("login-btn");
 login.addEventListener("click", () => {
   const userName = document.getElementById("username").value;
-console.log(userName)
 localStorage.setItem("txtValue", userName);
 });
