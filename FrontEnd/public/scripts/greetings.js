0af2761607ee7faa2fae2b2b5b57dfaba58344ea
Nomcebo-1 <ndimandenomcebo1@gmail.com>
2022-04-25 11:03:08 +0200

    dashboard

--- FrontEnd/public/scripts/greetings.js
+++ FrontEnd/public/scripts/greetings.js
@@ -10,7 +10,7 @@ console.log (admins[Answer])
 document.getElementById("Welcome-msg").innerHTML = "Welcome, " + admins[Answer] + "."
 document.getElementById("Welcome-msg").style.color = "rgb(49, 189, 199";
 document.getElementById("Welcome-msg").style.fontSize = "2.5em";
-document.getElementById("Welcome-msg").style.marginLeft = "1.5em";
+document.getElementById("Welcome-msg").style.marginLeft = "1em";
 document.getElementById("Welcome-msg").style.marginBottom = "1.5em";
 
 
